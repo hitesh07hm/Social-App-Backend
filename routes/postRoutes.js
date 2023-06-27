5b1847493bfb69bf1767077699929689207b7ce8
Hitesh <mahajanhitesh10hm@gmail.com>
2023-06-27 22:12:05 +0530

    added view comments and user can add a comment over a post

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -9,9 +9,15 @@ import fetchAllPost from "../controller/user_post/fetchAllPost.js";
 import fetchMyPost from "../controller/user_post/fetchMyPost.js";
 import deleteMyPost from "../controller/user_post/deleteMyPost.js";
 
+import addComment from "../controller/user_post/addComments.js";
+import viewComments from "../controller/user_post/viewComments.js";
+
 router.post("/create-post", verifyJWTToken, createPost);
 router.get("/fetch-all-post", verifyJWTToken, fetchAllPost);
 router.get("/fetch-my-post", verifyJWTToken, fetchMyPost);
 router.get("/delete-my-post", verifyJWTToken, deleteMyPost);
+
+router.post("/add-comment", verifyJWTToken, addComment)
+router.get("/view-comments", verifyJWTToken, viewComments)
 
 export default router;
